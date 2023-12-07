3b41f27de7623780aa8a0183a0d5fd76f4e93917
kevinelong <kevinelong@gmail.com>
2023-12-07 08:01:54 -0800

    new todo working

--- scripts/new_todo.js
+++ scripts/new_todo.js
@@ -8,15 +8,33 @@ document.addEventListener("DOMContentLoaded", e => {
         .then(r => r.json())
         .then(cats => cats.forEach(c => catList.innerHTML += catOption(c)));
 
+    //// GOAL IS TO GENERATE AN ENCODED FORM TO SEND AS THE "body"    
     //userid=1&category=1&priority=Medium&description=Make+Tea&deadline=2023-12-06
 
     saveButton.addEventListener("click", e => {
-        const elements = document.querySelectorAll("main [name]");
-        const content = [...elements].map(e => `${e.name}=${ encodeURIComponent(e.value) }`).join("&");
 
-        fetch("http://localhost:8083/api/todos/", {method:"POST", body: content, headers:{
-            "Content-type": "application/x-www-form-urlencoded" //"application/json"
-        } }).then(r => location="todos.html");
+        // LOOP WAY OF GATHERING FORM DATA
+        const elements = document.querySelectorAll("main [name]");
+        const content = [...elements].map(e => `${encodeURIComponent(e.name)}=${encodeURIComponent(e.value)}`).join("&");
+
+        // LINEAR WAY OF GATHERING FORM DATA
+        // const content = new FormData();
+        // content.append("userid", userList.value);
+        // content.append("category", catList.value);
+        // content.append("priority", priorityList.value);
+        // content.append("description", description.value);
+        // content.append("deadline", deadline.value);
+        // console.log(content)
+        //// FormData() NOT WORKING WITH ANY OR NONE FOR CONTENT TYPE
+
+        fetch("http://localhost:8083/api/todos/", {
+            method: "POST", body: content,
+            headers: {
+                "Content-type": "application/x-www-form-urlencoded" //"application/json"
+            }
+        }).then(r => {
+            location="http://localhost:3000/todos.html"
+        });
 
     }); //END SAVE
 });//END LOADED
